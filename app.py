66597e59d811b3b5419c43a84bc0d9bbbc9bff6c
Mr. Oussama Sebrou <90466380+OussamaSEBROU@users.noreply.github.com>
2025-06-17 16:06:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -466,7 +466,7 @@ if st.session_state.main_page_selection == "Video Extraction":
 
                 # Copyable text area (with built-in copy icon)
                 #st.text_area(
-                    "Copyable Visual Content Summary",
+                    #"Copyable Visual Content Summary",
                    # value=extracted_text,
                   #  height=300,
                 #    key="copy_summary_area",
